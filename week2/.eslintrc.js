767966f2ffd73816bc56bec9774cce969e3252d3
Ignacio Crotez <ignacio.cortez@kodigo.org>
2023-08-18 00:46:20 -0600

    Implementando ESlint

---
+++ week2/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    // Aquí puedes configurar reglas personalizadas o anular las reglas existentes
+  },
+};
--- week2/src/rle.ts
+++ week2/src/rle.ts
@@ -19,7 +19,7 @@ export class RLE {
     let decoded = "";
     let count = "";
 
-    for (let char of input) {
+    for (const char of input) {
       if (isNaN(Number(char))) {
         decoded += char.repeat(Number(count) || 1);
         count = "";
